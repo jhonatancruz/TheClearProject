fac60be66f5439fcc5c4b43b486704d1e84d374a
Jhonatan Cruz <jcruz3@drew.edu>
2017-11-11 16:36:00 -0500

    created a login and register redirection

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from twilio import twiml
 from math import cos, asin, sqrt
 from cs50 import SQL
 
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app= Flask(__name__)
 
@@ -29,6 +34,66 @@ def after_request(response):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return("must provide username")
+
+        # Ensure password AND confirmation was submitted
+        elif not request.form.get("password") or not request.form.get("confirmation"):
+            return("must provide and confirm password!")
+
+        # Ensure password matches confirmation.
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return("passwords do not match!")
+
+        # Add username to  database, ensuring no 2 same usernames registered.
+        result = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=request.form.get(
+            "username"), hash=generate_password_hash(request.form.get("password")))
+        if not result:
+            return("username already exists!")
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return("must provide username")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return("must provide password")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return("invalid username and/or password")
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 @app.route('/test')
 def test():
