94aabe074b03fb7156b697f7f37341a30d34a677
Mohib Jafri <mjafri@nat-oitwireless-inside-vapornet100-c-30056.Princeton.EDU>
2017-11-11 23:21:14 -0500

    Multiple Location Support for Google Maps API

--- TheClearProject/static/js/maps.js
+++ TheClearProject/static/js/maps.js
@@ -1,3 +1,16 @@
+var map;
+var Markers = {};
+var infowindow;
+
+var beaches = [
+    ['Bondi Beach', -33.890542, 151.274856, 0],
+    ['Coogee Beach', -33.923036, 151.259052, 1],
+    ['Cronulla Beach', -34.028249, 151.157507, 2],
+    ['Manly Beach', -33.80010128657071, 151.28747820854187, 3],
+    ['Maroubra Beach', -33.950198, 151.259302, 4]
+];
+
+
 function myMap() {
   let styles = [
 
@@ -33,41 +46,53 @@ function myMap() {
    };
   var myCenter = {lat:  -0.3609324, lng: 35.9782985};// new google.maps.LatLng(-0.3609324,35.9782985);
   var mapCanvas = document.getElementById("googleMap");
-  var map = new google.maps.Map(mapCanvas, options);
+  map = new google.maps.Map(mapCanvas, options);
 
   setMarkers(map);
 
 };
 
 function setMarkers(map){
-  var beaches = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-  ];
 
-  for (var i = 0; i < beaches.length; i++ ){
+  infowindow = new google.maps.InfoWindow(); //{
+  //  content: beach[0]
+  //});
+
+  for (i = 0; i < beaches.length; i++ ){
     var beach = beaches[i];
     var marker = new google.maps.Marker({
         position: {lat: beach[1], lng: beach[2]},
         animation: google.maps.Animation.DROP,
-        map:map
-
-
+        map:map,
     });
 
-    marker.setmap(map);
+    //marker.setmap(map);
+    google.maps.event.addListener(marker, 'click', (function(marker, i){
+      return function(){
+        infowindow.setContent(beach[i][0]);
+				infowindow.setOptions({maxWidth: 200});
+				infowindow.open(map, marker);
+      }
+    }) (marker, i));
+    Markers[beaches[i][3]] = marker;
 
-    var infowindow = new google.maps.InfoWindow({
-      content: beach[0]
-    })
-  };
+  }
 
-  marker.addListener('click', function() {
-    infowindow.open(map, marker)
-  });
+  locate(0);
+}
+
+function locate(marker_id) {
+	var myMarker = Markers[marker_id];
+	var markerPosition = myMarker.getPosition();
+	map.setCenter(markerPosition);
+	google.maps.event.trigger(myMarker, 'click');
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+  //marker.addListener('click', function() {
+    //infowindow.open(map, marker)
+  //});
 
 
   // marker.setMap(map);
@@ -78,5 +103,3 @@ function setMarkers(map){
   // marker.addListener('click', function() {
   //       infowindow.open(map, marker)
   // });
-
-}
